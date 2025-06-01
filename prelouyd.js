f4e7c83317ccf5d1670ede4de4c7bdbc306391da
GnomosloG <romaderiie@gmail.com>
2025-06-01 22:16:54 +0300

    Add files via upload

---
+++ prelouyd.js
@@ -0,0 +1,27 @@
+// Скрипт для экрана загрузки
+document.addEventListener('DOMContentLoaded', function() {
+  const loadingOverlay = document.querySelector('.loading-overlay');
+  const character = document.querySelector('.loading-overlay-character');
+  
+  document.body.classList.add('loading-active');
+  
+  // Запускаем анимацию
+  setTimeout(() => {
+    character.style.left = '100%';
+  }, 50);
+  
+  // Скрываем экран 
+  setTimeout(() => {
+    loadingOverlay.style.opacity = '0';
+    setTimeout(() => {
+      loadingOverlay.style.display = 'none';
+      document.body.classList.remove('loading-active');
+    }, 500);
+  }, 2900);
+});
+
+// Остальные скрипты
+function toggleMenu() {
+  const menu = document.getElementById('subscribeMenu');
+  menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+}
